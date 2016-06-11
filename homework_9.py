20a2423550eaffe8d2b35ae20f67ee25cf62f7d8
Nucleus2014 <2014301020131@whu.edu.cn>
2016-06-11 12:31:27 +0800

    Add files via upload

---
+++ homework_9.py
@@ -0,0 +1,46 @@
+import math
+omega = []
+theta = []
+t = []
+omega_p = []
+theta_p = []
+g = 9.8
+l = 9.8
+F = 1.2
+q = 0.5
+Omega = (g/l)**0.5
+Omega_D = 2./3
+dt = 0.04
+omega.append(0)
+theta.append(0.2)
+t.append(0)
+omega_p.append(0)
+theta_p.append(0.2)
+for i in range(int(25000)):
+	tmp1 = omega[i]-g*math.sin(theta[i])*dt/l-q*omega[i]*dt+F*math.sin(t[i]*Omega_D)*dt
+	omega.append(tmp1)
+	tmp2 = theta[i]+omega[i+1]*dt
+	tmp3 = t[i]+dt
+	t.append(tmp3)
+	while abs(tmp2)>math.pi:
+		if tmp2>math.pi:
+			tmp2=tmp2-2*math.pi
+		else:
+			tmp2=tmp2+2*math.pi
+	theta.append(tmp2)
+for i in range(int(25000)):
+	for n in range(int(8000)):
+		if abs(t[i]-2*n*math.pi/Omega_D)<0.5*dt:
+			omega_p.append(omega[i])
+			theta_p.append(theta[i])
+from pylab import *
+figure(figsize=(12,10),dpi=100)
+title('The nonlinear, forced pendulum')
+ylabel('omega(radians/s)')
+xlabel('theta(radians)')
+xlim(-4,4)
+ylim(-2,1)
+xticks([-4,-2,0,2,4])
+yticks([-2,-1,0,1])
+scatter(theta_p,omega_p,c='blue')
+show()
